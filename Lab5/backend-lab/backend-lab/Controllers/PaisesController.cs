e4bbab1b50de83afba38f01a6c38d7f598180478
JavierPupo <javi408x@gmail.com>
2024-09-15 18:52:25 -0600

    Lab 5 Completado

---
+++ Lab5/backend-lab/backend-lab/Controllers/PaisesController.cs
@@ -0,0 +1,27 @@
+﻿using backend_lab.Handlers;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend_lab.Models;
+
+namespace backend_lab.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler paisesHandler;
+
+        public PaisesController()
+        {
+            paisesHandler = new PaisesHandler();
+        }
+
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = paisesHandler.ObtenerPaises();
+            return paises;
+        }
+
+    }
+}
